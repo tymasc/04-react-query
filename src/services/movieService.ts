57c2575c9571e759d6bd1f8948837a6ada4be4b6
ta_imk <timofejzagirnij@gmail.com>
2025-07-13 17:55:15 +0300

    FixService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
-import { MovieSearchResponse } from '../types/service';
+import { Movie } from '../types/movie';
 
 const ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 const API_URL = 'https://api.themoviedb.org/3/search/movie';
+
+interface MovieSearchResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export const fetchMovies = async (
   query: string,
